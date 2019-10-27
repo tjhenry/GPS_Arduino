e10d34cf787c239dabafb53ecf67d613337a8bf1
Tim Henry <tim@ubiquisoft.com>
2019-10-27 12:28:34 -0500

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,129 @@
+#include <Arduino.h>
+
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+// Declare method so ordering doesn't matter
+extern void displayInfo();
+extern void nearestAirport();
+
+/*
+   This sample sketch demonstrates the normal use of a TinyGPS++ (TinyGPSPlus) object.
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+void setup()
+{
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    if (gps.encode(ss.read())) {
+      if (gps.location.isUpdated()){
+        displayInfo();
+        nearestAirport();
+      }
+    }
+      
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
+
+void nearestAirport() {
+
+  static const double airport_lat = 35.0424114;
+  static const double airport_lng = -89.9766792;
+
+  double distance = TinyGPSPlus::distanceBetween(
+    gps.location.lat(), 
+    gps.location.lng(), 
+    airport_lat, 
+    airport_lng) / 1000;
+
+   unsigned long courseTo = (unsigned long)TinyGPSPlus::courseTo(
+    gps.location.lat(), gps.location.lng(), airport_lat, airport_lng);
+
+  String cardinal = TinyGPSPlus::cardinal(courseTo);
+
+  Serial.print(F("KMEM - Distance: "));
+  Serial.print(distance);
+  Serial.print(F(" Course: "));
+  Serial.print(courseTo);
+  Serial.print(F(" - "));
+  Serial.print(cardinal);
+  Serial.println();
+}
